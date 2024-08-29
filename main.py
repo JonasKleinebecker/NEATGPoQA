30bd5b2632307f8a83a69239eb853093a931a995
Jonas Kleinebecker <jonaskleinebecker@gmail.com>
2024-08-30 00:43:46 +0200

    Add Quantum fourier 3 qubit test case and statevector simulation

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import random
+import time
 import numpy as np
 import deap.tools.emo as emo
 from deap import creator, base, tools, algorithms
-from quantum_simulation import run_circuit_on_simulator, run_circuit_on_statevector_simulator, deutsch_jozsa_oracle_balanced, deutsch_jozsa_oracle_balanced_inverse, deutsch_jozsa_oracle_const_0, deutsch_jozsa_oracle_const_1
+from qiskit_aer.backends.compatibility import Statevector
+from quantum_simulation import run_circuit, generate_3_qubit_fourier_transform_statevectors
 
-gate_set = ["h", "x", "cx", "rx", "ry", "rz", "oracle"]
+gate_set = ["h", "x", "cx", "rx", "ry", "rz", "swap", "cp"]
 
 num_qubits = 3
 min_individual_length = 3
-max_individual_length = 15
-use_multi_objective = True
+max_individual_length = 30
+use_multi_objective = False
+simulator_type = "statevector"
+statevector_holder = generate_3_qubit_fourier_transform_statevectors()
 
 # Create an individual class to hold a list of gates
 if(use_multi_objective):
@@ -18,32 +22,35 @@ else:
     creator.create("minErrorFitness", base.Fitness, weights=(-1.0,))  
 creator.create("Individual", list, fitness=creator.minErrorFitness)
 
+def create_gate():
+    gate_name = random.choice(gate_set)
+    control_qubit = None  # Needs to be remembered to avoid duplicate control and target qubits
+
+    gate_args = []
+    gate_args.append(gate_name)
+    if gate_name in ["rx", "ry", "rz", "cp"]:
+        angle = np.random.uniform(0, 2 * np.pi)
+        gate_args.append(angle)
+    if gate_name in ["cx", "swap", "cp"]:
+        control_qubit = random.randint(0, num_qubits - 1)
+        gate_args.append(control_qubit)
+    if gate_name in ["h", "x", "y", "z", "cx", "cp", "swap", "rx", "ry", "rz"]:
+        target_qubit = random.randint(0, num_qubits - 1)
+        while control_qubit == target_qubit:
+            target_qubit = random.randint(0, num_qubits - 1)
+        gate_args.append(target_qubit)
+    return gate_args
+
 def create_individual():
     individual = []
 
     length = random.randint(min_individual_length, max_individual_length)  
 
     for _ in range(length):
-        gate_name = random.choice(gate_set)
-        
-        if gate_name == "cx":
-            qubit1 = random.randint(0, num_qubits - 1)
-            qubit2 = random.randint(0, num_qubits - 1)
-            while qubit2 == qubit1:  
-                qubit2 = random.randint(0, num_qubits - 1)
-            individual.append((gate_name, qubit1, qubit2))
-        elif gate_name in ["rx", "ry", "rz"]:  
-            qubit = random.randint(0, num_qubits - 1)
-            angle = np.random.uniform(0, 2 * np.pi) 
-            individual.append((gate_name, angle, qubit))
-        elif gate_name == "oracle":
-            individual.append([gate_name])
-        else:  
-            qubit = random.randint(0, num_qubits - 1)
-            individual.append((gate_name, qubit))
-
+        individual.append(create_gate())
     return creator.Individual(individual)
 
+# ------- Genetic operators -------
 def single_point_crossover(ind1, ind2):
     """Performs single-point crossover on two individuals."""
     if len(ind1) < 2 or len(ind2) < 2: 
@@ -66,7 +73,7 @@ def mutate_individual(individual, mut_pb, add_pb, del_pb, change_pb, param_pb):
                                          p=[add_pb, del_pb, change_pb, param_pb])
 
         if mutation_type == "add":
-            new_gate = create_individual()[0]  
+            new_gate = create_gate()  
             insert_point = random.randint(0, len(individual))
             individual.insert(insert_point, new_gate)
 
@@ -75,42 +82,64 @@ def mutate_individual(individual, mut_pb, add_pb, del_pb, change_pb, param_pb):
 
         elif mutation_type == "change":
             index_to_change = random.randrange(len(individual))
-            attempt_counter = 0
-            max_attempts = 10 # Prevent infinite loop
-            while individual[index_to_change][0] == "oracle" and attempt_counter < max_attempts:  # Oracles have no parameters to change
-                index_to_change = random.randrange(len(individual))
-                attempt_counter += 1
-            individual[index_to_change] = create_individual()[0]
+            individual[index_to_change] = create_gate()
 
         elif mutation_type == "params": 
             index_to_change = random.randrange(len(individual))
             gate_name, *args = individual[index_to_change]
-            if gate_name == "cx":
+            if gate_name in ["cx", "swap"]:
                 new_qubit1 = random.randint(0, num_qubits - 1)
                 new_qubit2 = random.randint(0, num_qubits - 1)
                 while new_qubit1 == new_qubit2: 
                     new_qubit2 = random.randint(0, num_qubits - 1)
-                individual[index_to_change] = (gate_name, new_qubit1, new_qubit2)
+                individual[index_to_change] = ([gate_name, new_qubit1, new_qubit2])
+
+            elif gate_name in ["cp"]:
+                new_angle = np.random.uniform(0, 2 * np.pi)
+                new_qubit1 = random.randint(0, num_qubits - 1)
+                new_qubit2 = random.randint(0, num_qubits - 1)
+                while new_qubit1 == new_qubit2: 
+                    new_qubit2 = random.randint(0, num_qubits - 1)
+                individual[index_to_change] = ([gate_name, new_angle, new_qubit1, new_qubit2])
 
             elif gate_name in ["rx", "ry", "rz"]:
                 new_angle = np.random.uniform(0, 2 * np.pi)
                 new_qubit = random.randint(0, num_qubits - 1)
-                individual[index_to_change] = (gate_name, new_angle, new_qubit)
+                individual[index_to_change] = ([gate_name, new_angle, new_qubit])
 
             else:
                 new_qubit = random.randint(0, num_qubits - 1)
-                individual[index_to_change] = (gate_name, new_qubit)
+                individual[index_to_change] = ([gate_name, new_qubit])
 
     return individual, # Comma is needed because the function should return a tuple due to DEAP requirements
 
-def calculate_fitness_spector_1998(individual, simulation_results, correct_states):
+#------- Helper functions -------
+def normalize_value_0_to_1(value, min_value, max_value):
+    return (value - min_value) / (max_value - min_value)
+
+def convert_statevectors_to_probabilities_dict(statevectors):
+    results_probabilities = []
+    for statevector in statevectors:    
+        statevector_array = np.asarray(statevector) # Working on the statevector directly is deprecated
+        probabilities_dict = {}
+        for i, amplitude in enumerate(statevector_array):
+            probability = abs(amplitude) ** 2
+            bitstring = format(i, f'0{num_qubits}b')  # Convert index to bitstring
+            probabilities_dict[bitstring] = probability
+        results_probabilities.append(probabilities_dict)
+    return results_probabilities
+
+#------- Single-objective fitness functions -------
+def calculate_spector_1998_fitness(individual, simulation_results, correct_states):
     hits = len(simulation_results)
     correctness = 0
-    accuracy = 0
-    for counts, correct_states_per_case in zip(simulation_results, correct_states):
+    if(isinstance(simulation_results[0],Statevector)):
+        results_probabilities = convert_statevectors_to_probabilities_dict(simulation_results)
+        simulation_results = results_probabilities
+    for simulation_result, correct_states_per_case in zip(simulation_results, correct_states):
+        accuracy = 0
         for state in correct_states_per_case:
-            accuracy += counts.get(state, 0)
-        accuracy /= 1024
+            accuracy += simulation_result.get(state, 0)
         if (accuracy >= 0.52):
             hits -= 1
         else:
@@ -123,46 +152,83 @@ def calculate_fitness_spector_1998(individual, simulation_results, correct_state
         fitness = correctness + hits
     return fitness,
 
-def calculate_fitness_correctness_error(individual, simulation_results, correct_states):
-    correctness = 0
-    for counts, correct_states_per_case in zip(simulation_results, correct_states):
+def calculate_custom_spector_1998_fitness(individual, simulation_results, correct_states):
+    hits = len(simulation_results)
+    error = 0
+    if(isinstance(simulation_results[0],Statevector)):
+        results_probabilities = convert_statevectors_to_probabilities_dict(simulation_results)
+        simulation_results = results_probabilities
+    for simulation_result, correct_states_per_case in zip(simulation_results, correct_states):
+        accuracy = 0
         for state in correct_states_per_case:
-            correctness += counts.get(state, 0)
-    correctness /= 1024 * len(simulation_results)
+            accuracy += simulation_result.get(state, 0)
+        error += 1 - accuracy
+        if (accuracy >= 0.52):
+            hits -= 1
+    error /= len(simulation_results)
+    fitness = error + hits
+    if fitness < 0.00001: # account for floating point errors
+        fitness = len(individual) / 1000
+    return fitness,
+
+def calculate_error_fitness(individual, simulation_results, correct_states):
+    correctness = 0
+    if(isinstance(simulation_results[0],Statevector)):
+        results_probabilities = convert_statevectors_to_probabilities_dict(simulation_results)
+        simulation_results = results_probabilities
+    for simulation_result, correct_states_per_case in zip(simulation_results, correct_states):
+        for state in correct_states_per_case:
+            correctness += simulation_result.get(state, 0)
+    correctness /= len(simulation_results) 
     return 1 - correctness,
 
-def calculate_fitness_correctness_error_and_size(individual, simulation_results, correct_states):
+def calculate_phase_aware_error_fitness(individual, simulation_statevectors, correct_statevectors): 
+    if not isinstance(simulation_statevectors[0], Statevector):
+        raise ValueError("The called 'calculate_phase_aware_error_fitness' function is only applicable for statevectors.")
     correctness = 0
-    for counts, correct_states_per_case in zip(simulation_results, correct_states):
-        for state in correct_states_per_case:
-            correctness += counts.get(state, 0)
-    correctness /= 1024 * len(simulation_results)
-    return 1 - correctness, len(individual) 
+    for simulation_statevector, correct_statevector in zip(simulation_statevectors, correct_statevectors.values()):
+        correctness += np.abs(np.dot(simulation_statevector, correct_statevector.conjugate())) 
+    correctness /= len(simulation_statevectors)
+    return 1 - correctness,
+
+#------- Multi-objective fitness functions -------
+def calculate_error_and_gate_count(individual, simulation_results, correct_states):
+    error = calculate_error_fitness(individual, simulation_results, correct_states)
+    return error[0], normalize_value_0_to_1(len(individual), 1, max_individual_length) # Even though the min_individual_length is 3, mutation can reduce the length to a min of 1
 
 toolbox = base.Toolbox()
-toolbox.register("calculate_fitness", calculate_fitness_correctness_error_and_size)
+toolbox.register("calculate_fitness", calculate_phase_aware_error_fitness)
 
-def evaluate_deutsch_josza_1_input_qubits(individual):
-    counts_balanced = run_circuit_on_simulator(individual, 2, 2)
-    counts_balanced_inverse = run_circuit_on_simulator(individual, 2, 3)
-    counts_const_0 = run_circuit_on_simulator(individual, 2, 0)
-    counts_const_1 = run_circuit_on_simulator(individual, 2 ,1)
+def evaluate_deutsch_josza_1_input_qubits_qasm(individual):
+    counts_balanced = run_circuit(simulator_type, individual, 2, 2)
+    counts_balanced_inverse = run_circuit(simulator_type, individual, 2, 3)
+    counts_const_0 = run_circuit(simulator_type, individual, 2, 0)
+    counts_const_1 = run_circuit(simulator_type, individual, 2 ,1)
     simulation_results = [counts_balanced, counts_balanced_inverse, counts_const_0, counts_const_1]
     correct_states = [["01", "11"], ["01", "11"], ["10","00"], ["10","00"]]
     return toolbox.calculate_fitness(individual, simulation_results, correct_states)
 
+
+
 def evaluate_deutsch_josza_2_input_qubits(individual):
-    counts_balanced_1 = run_circuit_on_simulator(individual, 3, 2)
-    counts_balanced_2 = run_circuit_on_simulator(individual, 3, 3)
-    counts_balanced_3 = run_circuit_on_simulator(individual, 3, 4)
-    counts_balanced_4 = run_circuit_on_simulator(individual, 3, 5)
-    counts_balanced_5 = run_circuit_on_simulator(individual, 3, 6)
-    counts_balanced_6 = run_circuit_on_simulator(individual, 3, 7)
-    counts_const_0 = run_circuit_on_simulator(individual, 3, 0)
-    counts_const_1 = run_circuit_on_simulator(individual, 3, 1)
-    simulation_results = [counts_balanced_1, counts_balanced_2, counts_balanced_3, counts_balanced_4, counts_balanced_5, counts_balanced_6, counts_const_0, counts_const_1]
+    results_const_0 = run_circuit(simulator_type, individual, 3, 0)
+    results_const_1 = run_circuit(simulator_type, individual, 3, 1)
+    results_balanced_1 = run_circuit(simulator_type, individual, 3, 2)
+    results_balanced_2 = run_circuit(simulator_type, individual, 3, 3)
+    results_balanced_3 = run_circuit(simulator_type, individual, 3, 4)
+    results_balanced_4 = run_circuit(simulator_type, individual, 3, 5)
+    results_balanced_5 = run_circuit(simulator_type, individual, 3, 6)
+    results_balanced_6 = run_circuit(simulator_type, individual, 3, 7)
+    simulation_results = [results_balanced_1, results_balanced_2, results_balanced_3, results_balanced_4, results_balanced_5, results_balanced_6, results_const_0, results_const_1]
     correct_states = [["001","010","101","110","011", "111"], ["001","010","101","110","011", "111"], ["001","010","101","110","011", "111"], ["001","010","101","110","011", "111"], ["001","010","101","110","011", "111"], ["001","010","101","110","011", "111"], ["100","000"], ["100","000"],]
     return toolbox.calculate_fitness(individual, simulation_results, correct_states)
+
+def evaluate_quantum_fourier_3_qubits(individual):
+    simulation_results = []
+    for i in range (8):
+        initial_state_binary = format(i, '0' + str(3) + 'b')
+        simulation_results.append(run_circuit(simulator_type, individual, 3, None, initial_state_binary))
+    return toolbox.calculate_fitness(individual, simulation_results, statevector_holder)
 
 toolbox.register("individual", create_individual)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual) 
@@ -175,11 +241,12 @@ param_pb = 0.25
 
 toolbox.register("mutate", mutate_individual, mut_pb=mut_pb, add_pb=add_pb,
                  del_pb=del_pb, change_pb=change_pb, param_pb=param_pb)
-toolbox.register("select", tools.selNSGA2)
-toolbox.register("evaluate", evaluate_deutsch_josza_2_input_qubits)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("evaluate", evaluate_quantum_fourier_3_qubits)
 
-pop_size = 100
-num_generations = 10
+pop_size = 1000
+num_generations = 100
+num_elites = 25
 
 def run_gp():
     best_ind = None
@@ -190,9 +257,12 @@ def run_gp():
         ind.fitness.values = fit
 
     for g in range(num_generations):
-        print(f"-- Generation {g} --")
+        print(f"-- Generation {g + 1} --")
+        start_time_gp = time.time()
 
-        offspring = toolbox.select(pop, len(pop))
+        elites = tools.selBest(pop, num_elites)
+
+        offspring = toolbox.select(pop, len(pop) - num_elites)
         offspring = list(map(toolbox.clone, offspring))
 
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
@@ -205,19 +275,21 @@ def run_gp():
                 del mutant.fitness.values
 
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        start_time_sim = time.time()
         fitnesses = map(toolbox.evaluate, invalid_ind)
+        print(f"  Time taken for simulation: {time.time() - start_time_sim:.2f}s")
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit 
 
-        pop[:] = offspring
+        pop[:] = offspring + elites
 
         # print fitness statistics
         num_objectives = len(pop[0].fitness.values)
         for i in range(num_objectives):
             fits = [ind.fitness.values[i] for ind in pop]
-            print(f"  Min error of objective {i}: {min(fits)}")
-            print(f"  Max error of objective {i}: {max(fits)}")
-            print(f"  Avg error of objective {i}: {np.mean(fits)}")
+            print(f"  Min error of objective {i + 1}: {min(fits)}")
+            print(f"  Max error of objective {i + 1}: {max(fits)}")
+            print(f"  Avg error of objective {i + 1}: {np.mean(fits)}")
     
         # keep track of the best individual in single objective case
         if num_objectives == 1:
@@ -225,21 +297,25 @@ def run_gp():
             if best_ind is None or curr_best_ind.fitness.values[0] < best_ind.fitness.values[0]:
                 best_ind = curr_best_ind
 
+        print(f"  Time taken for generation: {time.time() - start_time_gp:.2f}s")
+
     if num_objectives > 1:
         selected_pop = tools.selNSGA2(pop, len(pop))
         # Perform non-dominated sorting to extract the Pareto front
         pareto_fronts = tools.sortNondominated(selected_pop, len(selected_pop))
         best_ind = pareto_fronts[0] 
     if(num_generations > 0):
-        print("Best individuals:", best_ind)
-        for ind_idx, ind in enumerate(best_ind):
-            for obj_idx ,obj_value in enumerate(ind.fitness.values):
-                print(f"Best individual {ind_idx + 1}, objective {obj_idx + 1}: {obj_value}")
-        
-        '''
-        print("best_ind spector_1998: ",toolbox.evaluate(best_ind))
-        toolbox.register("calculate_fitness", calculate_fitness_correctness_error)
-        print("best_ind correctness error: ",toolbox.evaluate(best_ind))
-        '''
+        if(num_objectives == 1):
+            print(f"Best individual: {best_ind}")
+            print(f"Best individual error: {best_ind.fitness.values[0]}")
+        else:
+            for ind_idx, ind in enumerate(best_ind):
+                for obj_idx ,obj_value in enumerate(ind.fitness.values):
+                    print(f"Best individual {ind_idx + 1}, {ind}")
+                    print(f"Best individual {ind_idx + 1}, objective {obj_idx + 1}: {obj_value}")
+                print(f"Individual {ind_idx + 1} gate count: {len(ind)}")
 
+quantum_fourier_3_individual = [["h", 2], ["cp", np.pi/2, 1, 2], ["h", 1], ["cp", np.pi/4, 0, 2], ["cp",np.pi/2, 0, 1], ["h", 0], ["swap", 0, 2]]
+deutsch_josza_3_individual = [('h', 0), ('h', 1), ('x', 2), ('h', 2), ['oracle'], ('h', 0), ('h', 1)]
+print(f"Known solution error: {toolbox.evaluate(quantum_fourier_3_individual)}")
 run_gp()
--- quantum_simulation.py
+++ quantum_simulation.py
@@ -1,48 +1,18 @@
+from numpy import pi
 from qiskit.circuit.library import UnitaryGate
 from qiskit import QuantumCircuit, QuantumRegister
 from qiskit_aer import QasmSimulator, StatevectorSimulator
 import random
 import numpy as np
 
-def deutsch_jozsa_oracle_const_0(state):
-    """Oracle function for the constant 0 function."""
-    return 0
+num_shots = 1024
 
-def deutsch_jozsa_oracle_const_1(state):
-    """Oracle function for the constant 1 function."""
-    return 1
-
-def deutsch_jozsa_oracle_balanced(state):
-    """Oracle function for the balanced function."""
-    return sum((map(int, state))) % 2 
-
-def deutsch_jozsa_oracle_balanced_inverse(state):
-    """Oracle function for the balanced function."""
-    return (sum((map(int, state))) + 1) % 2 
-
-def unity_oracle(state):
-    """Oracle function for the unity function."""
-    return 1
-
-def create_oracle_gate(num_qubits, oracle_type):
-    """Creates a custom Oracle gate from a given function."""
-    matrix_size = 2 ** num_qubits  
-    unitary_matrix = np.eye(matrix_size)
-    if(oracle_type == 1):
-        unitary_matrix[[0,1,2,3]] = unitary_matrix[[1,0,3,2]]
-    elif(oracle_type == 2):
-        unitary_matrix[[0,1,2,3]] = unitary_matrix[[0,3,2,1]]
-    elif(oracle_type == 3):
-        unitary_matrix[[0,1,2,3]] = unitary_matrix[[2,0,3,1]]
-
-    return UnitaryGate(unitary_matrix, label="Oracle") 
-
-def create_deutsch_josza_oracle_gate(num_input_qubits, oracle_type):
+def create_deutsch_josza_oracle_gate(num_input_qubits, oracle_case):
     """
     Creates a quantum gate implementing a Deutsch-Jozsa oracle.
 
     Args:
-        oracle_type (int): Specifies the type of oracle to create. 
+        oracle_type (int): Specifies the case of oracle to create. 
                            0 = constant 0 function
                            1 = constant 1 function
                            2-8 = different balanced functions
@@ -59,18 +29,18 @@ def create_deutsch_josza_oracle_gate(num_input_qubits, oracle_type):
     qr = QuantumRegister(total_qubits, 'q')
     qc = QuantumCircuit(qr)
 
-    if oracle_type == 0:
+    if oracle_case == 0:
         pass  
 
-    elif oracle_type == 1:
+    elif oracle_case == 1:
         qc.x(qr[total_qubits-1])  # Flip the output qubit to 1
 
-    elif oracle_type in range(2, 8):
+    elif oracle_case in range(2, 8):
         if num_input_qubits == 1:
-            if(oracle_type > 3):
+            if(oracle_case > 3):
                 raise ValueError("Invalid oracle_type. For the 1 input qubit variant there are only 4 possible functions.")
             outputs_map = [[0, 1], [1, 0]]  # For 1 input qubit
-            outputs = outputs_map[oracle_type - 2]
+            outputs = outputs_map[oracle_case - 2]
             if outputs[0] == 1:
                 qc.cx(qr[0], qr[total_qubits - 1]) 
         else:  
@@ -81,7 +51,7 @@ def create_deutsch_josza_oracle_gate(num_input_qubits, oracle_type):
                 [1, 0, 0, 1], 
                 [1, 0, 1, 0], 
                 [1, 1, 0, 0]]
-            outputs = outputs_map[oracle_type - 2]
+            outputs = outputs_map[oracle_case - 2]
 
         for i, output in enumerate(outputs):
             if output == 1:
@@ -94,48 +64,68 @@ def create_deutsch_josza_oracle_gate(num_input_qubits, oracle_type):
 
     return qc
 
-def run_circuit_on_simulator(individual, num_qubits, oracle_type):
+def run_circuit(simulator_type, individual, num_qubits, oracle_case, initial_bit_state = "0"):
     """Converts a DEAP individual into a Qiskit circuit and runs it on a simulator."""
-    qc = QuantumCircuit(num_qubits, num_qubits) # number of qubits, number of classical bits
+    if simulator_type == "qasm":
+        qc = QuantumCircuit(num_qubits, num_qubits)
+    elif simulator_type == "statevector":
+        qc = QuantumCircuit(num_qubits)
+    else:
+        raise ValueError("Invalid simulator_type. Choose 'qasm' or 'statevector'.") 
+
+    # Prepare the initial state
+    for j, bit in enumerate(initial_bit_state):
+        if bit == '1':
+            qc.x(j)
 
     # Apply gates from the individual to the circuit
     for gate_info in individual:
         gate_name, *args = gate_info
-        if gate_name == "cx":
-            qc.cx(*args)  
-        elif gate_name in ["rx", "ry", "rz"]:
-            getattr(qc, gate_name)(*args)  
-        elif gate_name == "oracle":
-            oracle_qc = create_deutsch_josza_oracle_gate(num_qubits - 1, oracle_type)
+        if gate_name == "oracle":
+            oracle_qc = create_deutsch_josza_oracle_gate(num_qubits - 1, oracle_case)
             qc.compose(oracle_qc, qubits=range(num_qubits), inplace=True)
         else:
             getattr(qc, gate_name)(*args)  
-    qc.measure(range(num_qubits), range(num_qubits))
-    simulator = QasmSimulator()
-    job = simulator.run(qc, shots=1024)
-    counts = job.result().get_counts(qc) 
-    return counts
+    
+    if simulator_type == "qasm":
+        qc.measure(range(num_qubits), range(num_qubits))
+        simulator = QasmSimulator()
+        job = simulator.run(qc, shots=num_shots)
+        counts = job.result().get_counts(qc) 
+        for key in counts:
+            counts[key] /= num_shots
+        return counts
+    elif simulator_type == "statevector":
+        simulator = StatevectorSimulator()
+        job = simulator.run(qc)
+        statevector = job.result().get_statevector(qc) 
+        return statevector
 
-def run_circuit_on_statevector_simulator(individual, num_qubits, oracle_function):
-    """Converts a DEAP individual into a Qiskit circuit and runs it on a statevector simulator."""
-    qc = QuantumCircuit(num_qubits)  # Only qubits are needed, no classical bits
+def generate_3_qubit_fourier_transform_statevectors():
+    """Prints the statevectors of the 3-qubit Fourier transform for all possible input states."""
+    num_qubits = 3
+    statevectors = {}
+    for i in range(2**num_qubits):
+        # Convert the integer 'i' to its binary representation
+        initial_state_binary = format(i, '0' + str(num_qubits) + 'b')
+        qc = QuantumCircuit(num_qubits)
 
-    # Apply gates from the individual to the circuit
-    for gate_info in individual:
-        gate_name, *args = gate_info
-        if gate_name == "cx":
-            qc.cx(*args) 
-        elif gate_name in ["rx", "ry", "rz"]:
-            getattr(qc, gate_name)(*args) 
-        elif gate_name == "oracle":
-            oracle_qc = create_oracle_gate(num_qubits, oracle_function)
-            qc.compose(oracle_qc, qubits=range(num_qubits), inplace=True)
-        else:
-            getattr(qc, gate_name)(*args) 
+        # Initialize the qubits to the corresponding initial state
+        for j, bit in enumerate(initial_state_binary):
+            if bit == '1':
+                qc.x(j)
 
-    # No measurement is performed 
+        # Apply the 3-qubit QFT
+        qc.h(2)
+        qc.cp(pi/2, 1, 2)
+        qc.h(1)
+        qc.cp(pi/4, 0, 2)
+        qc.cp(pi/2, 0, 1)
+        qc.h(0)
+        qc.swap(0, 2) 
 
-    simulator = StatevectorSimulator()
-    job = simulator.run(qc)
-    statevector = job.result().get_statevector(qc) 
-    return statevector
+        simulator = StatevectorSimulator()
+        job = simulator.run(qc)
+        statevector = job.result().get_statevector(qc) 
+        statevectors[initial_state_binary] = statevector
+    return statevectors
